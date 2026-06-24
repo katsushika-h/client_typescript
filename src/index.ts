e8182a0fd68132b214284d7ec516cf9193851037
Zen <chromamonster@gmail.com>
2026-06-25 00:22:27 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+
+app.use(express.static("."));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
