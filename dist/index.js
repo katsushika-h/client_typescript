0404805ddae365dd4cb3d8ddb88d633f1619908c
katsushika-h <chromamonster@gmail.com>
2026-07-06 00:52:42 +0800

    macbook commit

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,13 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+// Health check endpoint
+app.get("/healthz", (req, res) => {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.status(200).send("OK. The quick brown fox");
+    console.log("Health check endpoint accessed");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
-
+import { Request, Response } from "express";
 const app = express();
 const PORT = 8080;
 
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Health check endpoint
+app.get("/healthz", (req: Request, res: Response) => {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.status(200).send("OK. The quick brown fox");
+    console.log("Health check endpoint accessed");
+});
+
+//const middlewareLogResponses = (req: Request, res: Response, next: Function) => {
+//    if (res.statusCode >= 400) {
